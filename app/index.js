330d9ec50baa85a548792d51ff581215f8c90e63
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-16 19:26:50 -0300

    feat: parse NDJSON
    Even if it occurs to have two chunks in the same transmission we can handle that

--- app/index.js
+++ app/index.js
@@ -8,6 +8,7 @@ async function consumeAPI(signal) {
   let counter = 0;
   const reader = response.body
     .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON())
     .pipeTo(new WritableStream({
       write(chunk) {
         console.log(++counter, chunk)
@@ -15,6 +16,23 @@ async function consumeAPI(signal) {
     }));
 
   return reader;
+}
+
+function parseNDJSON() {
+  let ndjsonBuffer = '';
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk;
+      const items = ndjsonBuffer.split('\n');
+      items.slice(0, -1)
+        .forEach(item => controller.enqueue(JSON.parse(item)))
+      ndjsonBuffer = items[items.length - 1];
+    },
+    flush(controller) {
+      if (!ndjsonBuffer) return;
+      controller.enqueue(JSON.parse(ndjsonBuffer));
+    }
+  });
 }
 
 const abortController = new AbortController();
