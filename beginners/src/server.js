c415326eece2b15b9958a03ff05602c4fdf9b02c
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-17 09:48:11 -0300

    feat: generating data on-demand
    Using run() to generate data. For each data, I'll pass it forward. There's no need to process it all.

--- beginners/src/server.js
+++ beginners/src/server.js
@@ -1,9 +1,38 @@
 import { createServer } from 'node:http';
+import { Readable } from 'node:stream';
+import { randomUUID } from 'node:crypto';
 
 const PORT = 3333;
 
-createServer(async (request, response) => {
+function* run() {
+  for (let i = 0; i <= 99; i++) {
+    const data = {
+      id: randomUUID(),
+      name: `Samuel-${i}`,
+    };
+    yield data;
+  }
+}
 
+/**
+ * @param request it is like the Readable stream
+ * @param response it is like the Writable stream
+ * We can use a Transform in the middle
+ */
+createServer(async (request, response) => {
+  const readable = new Readable({ // String protocol
+    read() {
+      for (const data of run()) {
+        console.log('sending...', data);
+        this.push(JSON.stringify(data) + '\n');
+      }
+
+      this.push(null); // Data has ended
+    },
+  })
+
+  readable
+    .pipe(response)
 })
   .listen(PORT)
   .on('listening', () => console.log(`server is listening at ${PORT}`))
